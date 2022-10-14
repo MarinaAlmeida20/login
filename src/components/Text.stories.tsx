ee30649a3a5e446fdf29afe4c847f9bc50407594
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-10-14 18:28:05 +0100

    storybook component

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,8 +1,27 @@
-import { Text } from './Text'
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from './Text'
 
+// global
 export default {
     title: 'Components/Text',
-    component: Text
+    component: Text,
+    args: {
+        children: 'Hello Again'
+    }
+} as Meta<TextProps>
+
+
+// variations
+export const Default: StoryObj = {}
+
+export const Small: StoryObj = {
+    args: {
+        size: 'sm'
+    }
 }
 
-export const Default = {}
+export const Large: StoryObj = {
+    args: {
+        size: 'lg'
+    }
+}
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,5 +1,21 @@
-export function Text() {
+import { clsx } from 'clsx'
+
+export interface TextProps {
+    size?: 'sm' | 'md' | 'lg';
+    children: string;
+}
+
+export function Text({ size = 'md', children }: TextProps) {
     return (
-        <span className="text-gray-100">Hello All</span>
+        <span className={clsx(
+            'text-gray-100 font-sans',
+            {
+                'text-xs': size === 'sm',
+                'text-sm': size === 'md',
+                'text-md': size === 'lg',
+            }
+        )}>
+            {children}
+        </span>
     )
 }
