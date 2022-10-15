09e9cd13273f02aae30b5537bca725875e7f7db1
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-10-15 19:25:52 +0100

    add msw

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -16,6 +16,7 @@ module.exports = {
     storyStoreV7: true,
     interactionsDebugger: true,
   },
+  staticDirs: ["../public"],
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
       config.base = "/login/";
