ff4f89f56a9100df9e7d4e77778b1438eb496a19
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-10-15 17:54:57 +0100

    create pages, add signin

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { SignIn } from './SignIn'
+
+// global
+export default {
+    title: 'Pages/Sign in',
+    component: SignIn,
+    args: {},
+    argTypes: {}
+} as Meta
+
+
+// variations
+export const Default: StoryObj = {}
